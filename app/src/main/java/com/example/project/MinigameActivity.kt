c32e3763a7ef8f6dc6f83a14446da35f7ddbe007
SamDiehl <32347010+samdiehl97@users.noreply.github.com>
2019-11-20 13:52:38 -0500

    Added Minigame Activity

---
+++ app/src/main/java/com/example/project/MinigameActivity.kt
@@ -0,0 +1,13 @@
+package com.example.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+
+class MinigameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
