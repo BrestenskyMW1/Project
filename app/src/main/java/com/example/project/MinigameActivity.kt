d90bce13d65dca562c7e5bb27b910e084d197bbb
SamuelDiehl <47796568+SamuelDiehl@users.noreply.github.com>
2019-11-20 15:50:21 -0500

    Added binding and layout

--- app/src/main/java/com/example/project/MinigameActivity.kt
+++ app/src/main/java/com/example/project/MinigameActivity.kt
@@ -3,11 +3,16 @@ package com.example.project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.example.project.databinding.ActivityMinigameBinding
 
 class MinigameActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMinigameBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_minigame)
 
     }
 }
