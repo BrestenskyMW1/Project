15394628d1f6d528c0a12a12f079be35f8170f26
Nimitz <MumawBM1@gcc.edu>
2019-11-20 17:21:28 -0500

    Revert "coroutines start"
    This reverts commit adc11ad52cbce50e0f313da609c4b07993c9ce27.

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -8,20 +8,16 @@ import android.view.View
 import androidx.drawerlayout.widget.DrawerLayout
 import android.widget.Button
 import android.widget.EditText
-import androidx.room.Database
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 
 class MainActivity : AppCompatActivity(){
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var mBtLaunchActivity: Button
-    private lateinit var database: Database;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        database =
+
     }
 
     fun sendMessage(view: View) {
@@ -32,11 +28,4 @@ class MainActivity : AppCompatActivity(){
         }
         startActivity(intent)
     }
-
-    private suspend fun load(){
-        withContext(Dispatchers.IO){
-            database.clear();
-        }
-    }
-
 }
