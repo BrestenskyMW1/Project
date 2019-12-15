87884dc0ea683a427c3e610336eb8c11e435eb11
BrestenskyMW1 <44762439+BrestenskyMW1@users.noreply.github.com>
2019-12-15 13:32:01 -0500

    Code cleanup

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -53,23 +53,9 @@ class MainActivity : AppCompatActivity(){
         msgViewModel= ViewModelProviders.of(
             this, viewModelFactory).get(MsgViewModel::class.java)
 
-
-        //fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         //get permissions if not given already
         val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
         ActivityCompat.requestPermissions(this, permissions,0)
-
-        //stuff i added to fix the lat/lon updating
-        //locationRequest = LocationRequest.create()
-        //locationCallback = object : LocationCallback() {
-            //override fun onLocationResult(locationResult: LocationResult?) {
-                //locationResult ?: return
-                //for (location in locationResult.locations){
-                    //startLocationUpdates()
-                    //getLocation()
-                //}
-            //}
-        //}
 
         locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
         getLocation()
@@ -84,9 +70,7 @@ class MainActivity : AppCompatActivity(){
         //create a new message
         subButton.setOnClickListener {
             Toast.makeText(this@MainActivity, textEntry.getText().toString(), Toast.LENGTH_SHORT).show()
-            //startLocationUpdates()
             getLocation()
-            //Toast.makeText(this@MainActivity, msgViewModel.getLat().toString(), Toast.LENGTH_SHORT).show()
             msgViewModel.newMessage(textEntry.getText().toString())
         }
 
@@ -140,27 +124,6 @@ class MainActivity : AppCompatActivity(){
             //nothing
         }
     }
-    /*override fun onPause() {
-        super.onPause()
-        stopLocationUpdates()
-    }
-    fun getLocation(){
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location : Location? ->
-                // Got last known location. In some rare situations this can be null.
-                if (location != null) {
-                    msgViewModel.setLat(location.getLatitude());
-                    msgViewModel.setLon(location.getLongitude());
-                }
-            }
-    }
-    private fun startLocationUpdates() {
-        fusedLocationClient.requestLocationUpdates(locationRequest,
-            locationCallback,
-            Looper.getMainLooper())
-    }
-    private fun stopLocationUpdates() {
-        fusedLocationClient.removeLocationUpdates(locationCallback)
-    }*/
+
 
 }
